3e90517f27b7d54ac90c2ae56559d2521e36ef03
Axel Gathelier <130740013+GolfOcean334@users.noreply.github.com>
2025-01-14 17:13:27 +0100

    Responsive

--- Web Site/Assets/Script/OpenURL.cs
+++ Web Site/Assets/Script/OpenURL.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class OpenURL : MonoBehaviour
 {
-    // Cette fonction sera appelée lorsque le bouton sera cliqué
     public void OpenGoogle()
     {
         Application.OpenURL("https://www.google.com");
